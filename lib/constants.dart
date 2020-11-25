0a02652d1ccf9fd3c8aad57fd8920c21e8d6b40d
Enmanuel Veras <enmaveras07@gmail.com>
2020-11-25 09:51:42 -0400

    Project setup

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+library constants;
+
+import 'package:flutter/material.dart';
+
+const BACKGROUND_COLOR = Color(0xFF091C40);
+const SECONDARY_COLOR = Color(0xFF606060);
+const RED_COLOR = Color(0xFFFF1E46);
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'package:voice_chat_flutter/screens/audioCallWithImage/audio_call_with_image_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: AudioCallWithImage(),
+    );
+  }
+}
